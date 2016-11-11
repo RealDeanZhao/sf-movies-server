f92d792ec45ae03af4dfc55983558dc50577962a
Dean Zhao <zestxjest@outlook.com>
2016-11-11 19:03:23 +0800

    refactoring...and fix some bug

--- DAL/movie.js
+++ DAL/movie.js
@@ -31,56 +31,86 @@ exports.list = function (limit, page, callback) {
 
 exports.get = function (id, callback) {
     var result = {};
-    Movie.filter({ id: req.params.id })
-        .run()
-        .then(function (data) {
-            result.data = data;
-            callback(result);
-        }).error(function (err) {
-            result.data = [];
-            result.error = err;
-            callback(result);
-        });
+    Movie.get(id).then(function (data) {
+        result.data = data;
+        callback(result);
+    }).error(function (err) {
+        result.data = {};
+        result.error = err;
+        callback(result);
+    });
 }
 
-exports.create = function (id, callback) {
+exports.create = function (movie, callback) {
     var result = {};
-    Movie.filter({ id: req.params.id })
-        .run()
-        .then(function (data) {
-            result.data = data;
-            callback(result);
-        }).error(function (err) {
-            result.data = [];
-            result.error = err;
-            callback(result);
-        });
+    var movie = new Movie({
+        title: movie.title,
+        release_year: movie.release_year,
+        locations: movie.locations,
+        fun_facts: movie.fun_facts,
+        production_company: movie.production_company,
+        distributor: movie.distributor,
+        director: movie.director,
+        writer: movie.writer,
+        actor_1: movie.actor_1,
+        actor_2: movie.actor_2,
+        actor_3: movie.actor_3
+    });
+
+    movie.save().then(function (data) {
+        result.data = data;
+        callback(result);
+    }).error(function (err) {
+        result.data = {};
+        result.error = err;
+        callback(result);
+    });
 }
 
-exports.update = function (id, callback) {
+exports.update = function (id, newMovie, callback) {
     var result = {};
-    Movie.filter({ id: req.params.id })
-        .run()
-        .then(function (data) {
+
+    Movie.get(id).then(function (movie) {
+        movie.title = newMovie.title;
+        movie.release_year = newMovie.release_year;
+        movie.locations = newMovie.locations;
+        movie.fun_facts = newMovie.fun_facts;
+        movie.production_company = newMovie.production_company;
+        movie.distributor = newMovie.distributor;
+        movie.director = newMovie.director;
+        movie.writer = newMovie.writer;
+        movie.actor_1 = newMovie.actor_1;
+        movie.actor_2 = newMovie.actor_2;
+        movie.actor_3 = newMovie.actor_3;
+        movie.save().then(function (data) {
             result.data = data;
             callback(result);
         }).error(function (err) {
-            result.data = [];
+            result.data = {};
             result.error = err;
             callback(result);
         });
+    }).error(function (err) {
+        result.data = {};
+        result.error = err;
+        callback(result);
+    });
 }
 
 exports.delete = function (id, callback) {
     var result = {};
-    Movie.filter({ id: req.params.id })
-        .run()
-        .then(function (data) {
+    Movie.get(id).then(function (movie) {
+        movie.delete().then(function (data) {
             result.data = data;
             callback(result);
         }).error(function (err) {
-            result.data = [];
             result.error = err;
+            result.data = {};
             callback(result);
-        });
+        })
+    }).error(function (err) {
+        result.data = {};
+        result.error = err;
+        callback(result);
+    });
 }
--- routes/movies.js
+++ routes/movies.js
@@ -11,80 +11,52 @@ router.get('/', function (req, res, next) {
 
     movieDAL.list(limit, page, function (result) {
         if (result.error && result.error !== '') {
-            res.send(502, result.error);
+            res.status(502).send(result);
         } else {
-            res.send(302, result.data);
+            res.status(302).send(result);
         }
     });
 });
 
 router.get('/:id', function (req, res, next) {
-    movieDAL.get(req.params.id, function(result){
+    movieDAL.get(req.params.id, function (result) {
         if (result.error && result.error !== '') {
-            res.send(502, result.error);
+            res.status(502).send(result);
         } else {
-            res.send(302, result.data);
+            res.status(302).send(result);
         }
     });
 });
 
 router.post('/', function (req, res, next) {
-    var movie = new Movie({
-        title: req.body.title,
-        release_year: req.body.release_year,
-        locations: req.body.locations,
-        fun_facts: req.body.fun_facts,
-        production_company: req.body.production_company,
-        distributor: req.body.distributor,
-        director: req.body.director,
-        writer: req.body.writer,
-        actor_1: req.body.actor_1,
-        actor_2: req.body.actor_2,
-        actor_3: req.body.actor_3
-    });
-
-    movie.save().then(function (result) {
-        res.send(200, result)
-    }).error(function (err) {
-        res.send(502, err);
+    movieDAL.create(req.body, function (result) {
+        if (result.error && result.error !== '') {
+            res.status(502).send(result);
+        } else {
+            res.status(200).send(result);
+        }
     });
 });
 
 router.put('/:id', function (req, res, next) {
-    var movie = new Movie({
-        title: req.body.title,
-        release_year: req.body.release_year,
-        locations: req.body.locations,
-        fun_facts: req.body.fun_facts,
-        production_company: req.body.production_company,
-        distributor: req.body.distributor,
-        director: req.body.director,
-        writer: req.body.writer,
-        actor_1: req.body.actor_1,
-        actor_2: req.body.actor_2,
-        actor_3: req.body.actor_3,
-        id: req.params.id
-    });
-
-    movie.save().then(function (result) {
-        res.send(200, result)
-    }).error(function (err) {
-        res.send(502, err);
+    console.log(req.body);
+    movieDAL.update(req.params.id, req.body, function (result) {
+        if (result.error && result.error !== '') {
+            res.status(502).send(result);
+        } else {
+            res.status(200).send(result);
+        }
     });
 });
 
 router.delete('/:id', function (req, res, next) {
-    Movie.get(req.params.id).then(function (movie) {
-        movie.delete().then(function (result) {
-            res.send(200, result)
-        }).error(function (err) {
-            res.send(502, err)
-            console.log(err)
-        }).error(function (err) {
-            res.send(502, err)
-            console.log(err)
-        });
-    })
+    movieDAL.delete(req.params.id, function (result) {
+        if (result.error && result.error !== '') {
+            res.status(502).send(result.error);
+        } else {
+            res.status(200).send(result.data);
+        }
+    });
 });
 
 module.exports = router;
